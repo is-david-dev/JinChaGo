ecf8795655408ebcc8ca3dba3609af0c7481e137
David <125172926+is-david-dev@users.noreply.github.com>
2025-02-13 00:41:50 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// JavaScript
+document.getElementById('mainButton').addEventListener('click', function() {
+    const container = document.querySelector('.hidden-container');
+    container.classList.add('visible');
+  });
